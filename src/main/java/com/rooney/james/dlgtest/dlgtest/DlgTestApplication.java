aad276188a953b8e602b2c3447074c62db8e7f2b
jamesvrooney <jamesvrooney@hotmail.com>
2020-11-23 12:28:42 +0000

    Initial commit

---
+++ src/main/java/com/rooney/james/dlgtest/dlgtest/DlgTestApplication.java
@@ -0,0 +1,13 @@
+package com.rooney.james.dlgtest.dlgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlgTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooney/james/dlgtest/dlgtest/DlgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooney.james.dlgtest.dlgtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
